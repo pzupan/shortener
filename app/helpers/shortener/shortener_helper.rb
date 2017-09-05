d5c5767cb3036698cce380204e87aaffd2344bb8
pzupan <pzupan@gmail.com>
2017-09-05 15:57:55 -0700

    fix shortener-helper

--- app/helpers/shortener/shortener_helper.rb
+++ app/helpers/shortener/shortener_helper.rb
@@ -1,19 +1,22 @@
 module Shortener::ShortenerHelper
 
   # generate a url from a url string
-  def short_url(url, owner: nil, custom_key: nil, expires_at: nil, fresh: false, category: nil, url_options: {})
+  def short_url(url, **options)
+
+    options[:fresh] ||= false
+    options[:url_options] ||= {}
+    
     short_url = Shortener::ShortenedUrl.generate(
       url,
-      owner:      owner,
-      custom_key: custom_key,
-      expires_at: expires_at,
-      fresh:      fresh,
-      category:   category
+      owner:      options[:owner],
+      expires_at: options[:expires_at],
+      fresh:      options[:fresh],
+      category:   options[:category]
     )
 
     if short_url
-      options = { controller: :"/shortener/shortened_urls", action: :show, id: short_url.unique_key, only_path: false }.merge(url_options)
-      url_for(options)
+      attrs = { controller: :"/shortener/shortened_urls", action: :show, id: short_url.unique_key, only_path: false }.merge(options[:url_options])
+      url_for(attrs)
     else
       url
     end
